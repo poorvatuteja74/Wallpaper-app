1f9d415eb223339fe0aa6232b4d0c264ba3b9f4a
Poorva Tuteja <poorvatuteja@Poorvas-MacBook-Air.local>
2024-09-05 02:57:45 -0400

    added the filters

--- components/filtersModal.js
+++ components/filtersModal.js
@@ -1,32 +1,62 @@
-import React, { useCallback, useMemo, useRef } from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import React, { useMemo } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 import {
   BottomSheetModal,
   BottomSheetView,
   BottomSheetModalProvider,
 } from '@gorhom/bottom-sheet';
+import { BlurView } from 'expo-blur';
+import { Extrapolation, interpolate, useAnimatedStyle } from 'react-native-reanimated';
+import Animated from 'react-native-reanimated'; // Import Animated from react-native-reanimated
 
-const FiltersModal = ({modalRef}) => {
-  // ref
-//   const bottomSheetModalRef = useRef<BottomSheetModal>(null);
-
-//   // variables
+const FiltersModal = ({ modalRef }) => {
+  // Define snap points
   const snapPoints = useMemo(() => ['75%'], []);
 
   // renders
   return (
-        <BottomSheetModal
-          ref={modalRef}
-          index={0}
-          snapPoints={snapPoints}
-          enablePanDownToClose ={true}
-        //   onChange={handleSheetChanges}
-        
-        >
-          <BottomSheetView style={styles.contentContainer}>
-            <Text>Awesome 🎉</Text>
-          </BottomSheetView>
-        </BottomSheetModal>
+    <BottomSheetModal
+      ref={modalRef}
+      index={0}
+      snapPoints={snapPoints}
+      enablePanDownToClose={true}
+      backdropComponent={CustomBackdrop} // Use CustomBackdrop
+    >
+      <BottomSheetView style={styles.contentContainer}>
+        <Text>Awesome 🎉</Text>
+      </BottomSheetView>
+    </BottomSheetModal>
+  );
+};
+
+const CustomBackdrop = ({ animatedIndex, style }) => {
+
+  const containerAnimatedStyle = useAnimatedStyle(() => {
+    const opacity = interpolate(
+      animatedIndex.value,
+      [-1, 0],
+      [0, 1],
+      Extrapolation.CLAMP
+    );
+
+    return { opacity }; // Return opacity as an object
+  });
+
+  const containerStyle = [
+    StyleSheet.absoluteFill,
+    style,
+    styles.overlay,
+    containerAnimatedStyle,
+  ];
+
+  return (
+    <Animated.View style={containerStyle}>
+      <BlurView 
+        style={StyleSheet.absoluteFill}
+        tint='dark'
+        intensity={25}
+      />
+    </Animated.View>
   );
 };
 
@@ -40,6 +70,9 @@ const styles = StyleSheet.create({
   contentContainer: {
     flex: 1,
     alignItems: 'center',
+  },
+  overlay: {
+    backgroundColor: 'rgba(0,0,0,0.5)', // Correct the rgba format
   },
 });
 
